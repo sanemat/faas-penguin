504aa8807a47937b9932fe27b622c9de5136d392
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2021-04-18 23:17:22 +0700

    test(sam): move test

--- packages/faas-penguin-aws-lambda/__tests__/faas-penguin-aws-lambda.test.js
+++ packages/faas-penguin-aws-lambda/__tests__/faas-penguin-aws-lambda.test.js
@@ -1,3 +1,16 @@
-import { strict as assert } from "assert";
+const { faasPenguinAwsLambda } = require("../lib/faas-penguin-aws-lambda");
+const assert = require("assert/strict");
 
-assert.ok(true);
+(async () => {
+  let event = null;
+  let context = null;
+  const result = await faasPenguinAwsLambda(event, context);
+  assert.equal(typeof result, "object");
+  assert.equal(result.statusCode, 200);
+  assert.equal(typeof result.body, "string");
+
+  let response = JSON.parse(result.body);
+
+  assert.equal(typeof response, "object");
+  assert.equal(response.message, "hello world from lib");
+})();
---
+++ packages/faas-penguin-aws-lambda/app.js
@@ -0,0 +1,2 @@
+const { faasPenguinAwsLambda } = require("./lib/faas-penguin-aws-lambda");
+exports.lambdaHandler = faasPenguinAwsLambda;
